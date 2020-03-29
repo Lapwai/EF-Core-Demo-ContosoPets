85b9c9661333ee4390ca91bf3890dcad6bb49c55
lapwailau <lapwailau@gmail.com>
2020-03-30 03:13:45 +1300

    'Init'

---
+++ ContosoPets/Data/ContosoPetsContext.cs
@@ -0,0 +1,17 @@
+﻿using ContosoPets.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoPets.Data
+{
+    public class ContosoPetsContext:DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet <Product> Products { get; set; }
+        public DbSet <ProductOrder> productOrders { get; set; }
+    }
+}
---
+++ ContosoPets/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoPets.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+        
+
+
+        public ICollection<Order> Orders { get; set; }
+
+
+    }
+}
